9b53e6bad948406a9273ae968f85fa6928247bc4
Juliayade <julialapuyade@gmail.com>
2026-03-12 02:16:51 +0100

    Rename parameter 'request' to 'req' in GET function

--- app/api/download/route.js
+++ app/api/download/route.js
@@ -5,9 +5,9 @@ const downloads = {
   }
 };
 
-export async function GET(request) {
+export async function GET(req) {
 
-  const { searchParams } = new URL(request.url);
+  const { searchParams } = new URL(req.url);
   const token = searchParams.get("token");
 
   const data = downloads[token];
